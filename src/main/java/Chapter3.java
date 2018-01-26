f4cebe4ef74a9b5a07e62dc005f1dbea94f891ff
Dmitry Pashko <dmitry.pashko@inveritasoft.com>
2018-01-26 17:18:16 +0200

    Added Examples about Zip, Merge.

--- src/main/java/Chapter3.java
+++ src/main/java/Chapter3.java
@@ -1,11 +1,25 @@
+import com.sun.media.sound.InvalidFormatException;
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.Single;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.BiFunction;
+import io.reactivex.schedulers.Schedulers;
 import org.jsoup.Jsoup;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Iterator;
+import java.util.stream.IntStream;
 
 class Chapter3 {
 
     Chapter3() {
 //        filterExample();
 //        flatMapExample();
+//        flatMapExampleConcurrency();
+//        mergeExample();
+        zipExample();
     }
 
     private void filterExample() {
@@ -36,6 +50,7 @@ class Chapter3 {
     private Observable<String> getBody(String link) {
         HttpClient client = new HttpClient();
         return Observable.fromCallable(() -> client.sendGetRequest(link))
+                .observeOn(Schedulers.io())
                 .cast(String.class);
     }
 
@@ -46,4 +61,54 @@ class Chapter3 {
 
     }
 
+    /**
+     * Async parsing title from HTML body for each link, at one time not more that 3 requests.
+     */
+    private void flatMapExampleConcurrency() {
+        Disposable d = links().flatMap(this::getBody, 5)
+                .flatMap(this::getTitle)
+                .subscribe(title -> Logger.log("[%s]", title));
+        //Waiting for threads.
+        while (!d.isDisposed()) {
+        }
+    }
+
+    private void mergeExample() {
+        //Merge with [1..4] Observables.
+        Single.merge(getHash("goodvin1709"), getHash("Java"), getHash("RxJava"))
+                .subscribe(hash -> Logger.log("%s", hash));
+        Logger.log("=====================");
+        //Merge Observable with another.
+        getHash("goodvin1709").mergeWith(getHash("RxJava")).subscribe(hash -> Logger.log("%s", hash));
+
+    }
+
+    private Single<String> getHash(String value) {
+        return Single.create(emitter -> {
+            if (value != null) {
+                MessageDigest digest = MessageDigest.getInstance("SHA-256");
+                byte[] hash = digest.digest(value.getBytes(StandardCharsets.UTF_8));
+                digest.reset();
+                String result = IntStream.range(0, hash.length)
+                        .mapToObj(b -> Integer.toHexString(0xFF & hash[b]))
+                        .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                        .toString();
+                emitter.onSuccess(result);
+            } else {
+                emitter.onError(new InvalidFormatException("Invalid input value."));
+            }
+        });
+    }
+
+    /**
+     * Holy f**king shit :)
+     */
+    private void zipExample() {
+        String[] values = {"Goodvin1709","Java","RxJava"};
+
+        Observable.zip(Observable.fromArray(values),
+                Observable.fromArray(values).flatMap(val -> getHash(val).toObservable()),
+                (BiFunction<String, String, Object>) (s, s2) -> String.format("value[%s] - hash[%s]",s,s2))
+                .subscribe(v -> Logger.log("%s",v));
+    }
 }
