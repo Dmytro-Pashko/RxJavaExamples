d00680a39030476cfbd8811a361ea8012b66f153
Dmitry Pashko <dmitry.pashko@inveritasoft.com>
2018-03-29 02:16:46 +0300

    Added examples for Scan, Reduce, Collect, Disctinct, DistinctUntilChanging, Take, Skip, First, Last, TakeUntil, WhileUntil, ElementAt operators.

--- src/main/java/Chapter3.java
+++ src/main/java/Chapter3.java
@@ -1,15 +1,23 @@
+import com.sun.javafx.scene.control.skin.VirtualFlow;
 import com.sun.media.sound.InvalidFormatException;
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
+import io.reactivex.Scheduler;
 import io.reactivex.Single;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.BiFunction;
 import io.reactivex.schedulers.Schedulers;
 import org.jsoup.Jsoup;
+import sun.management.counter.Units;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 class Chapter3 {
@@ -19,7 +27,18 @@ class Chapter3 {
 //        flatMapExample();
 //        flatMapExampleConcurrency();
 //        mergeExample();
-        zipExample();
+//        zipExample();
+//        scanExample();
+//        reduceExample();
+//        creatingListWithReduce();
+//        distinctExample();
+//        distinctUntilChangedExample();
+//        takeExample();
+//        skipExample();
+//        firstLastExample();
+//        takeUntilExample();
+//        takeWhileExample();
+        elementAtExample();
     }
 
     private void filterExample() {
@@ -111,4 +130,129 @@ class Chapter3 {
                 (s, s2) -> String.format("value[%s] - hash[%s]", s, s2))
                 .subscribe(v -> Logger.log("%s", v));
     }
+
+    /**
+     * Accumulate previous value and current and emmit all intermediate values
+     */
+    private void scanExample() {
+        List<Integer> blocks = Arrays.asList(5, 5, 10, 10, 20, 50);
+        Observable.fromIterable(blocks)
+                .scan((accumulator, value) -> accumulator + value)
+                .blockingSubscribe(v -> Logger.log("%d", v));
+    }
+
+    /**
+     * Accumulate previous value and current and emmit only result
+     * But not working with infinity sources.
+     */
+    private void reduceExample() {
+        List<Integer> blocks = Arrays.asList(5, 5, 10, 10, 20, 50);
+        Observable.fromIterable(blocks)
+                .reduce((accumulator, value) -> accumulator + value)
+                .toObservable()
+                .blockingSubscribe(total -> Logger.log("%d", total));
+    }
+
+    /**
+     * Creating list from emmited object, for accumulator used ArrayList
+     * and Bifunction for each operation, required to return accumulator each time.
+     */
+    private void creatingListWithReduce() {
+        Single<List<Integer>> totalList = Observable.range(10, 10)
+                .reduce(new ArrayList<Integer>(), (list, current) -> {
+                    list.add(current);
+                    return list;
+                });
+        totalList.subscribe(list -> list.forEach(item -> Logger.log("%d", item)));
+    }
+
+    /**
+     * Simplify creating total collected list using collect operator, where accumulator the same
+     * for each action.
+     */
+    private void creatingListWithCollect() {
+        Single<List<Integer>> totalList = Observable.range(10, 10)
+                .collect(ArrayList::new, List::add);
+        totalList.subscribe(list -> list.forEach(item -> Logger.log("%d", item)));
+    }
+
+    /**
+     * Select only unique elements from all source.
+     */
+    private void distinctExample() {
+        List<String> values = Arrays.asList("A", "B", "C", "D", "A", "C", "B", "B", "D");
+        Observable.fromIterable(values)
+                .distinct()
+                .collect(ArrayList::new, List::add)
+                .subscribe(list -> list.forEach(item -> Logger.log("%s", item)));
+
+    }
+
+    /**
+     * Select emmit value when previous value not equals with current.
+     */
+    private void distinctUntilChangedExample() {
+        List<String> values = Arrays.asList("A", "A", "A", "B", "C", "D", "A", "C", "B", "B", "D");
+        Observable.fromIterable(values)
+                .distinctUntilChanged()
+                .collect(ArrayList::new, List::add)
+                .subscribe(list -> list.forEach(item -> Logger.log("%s", item)));
+
+    }
+
+    /**
+     * Emmit first n elements.
+     * when n is below 0 then will be throw java.lang.IllegalArgumentException
+     */
+    private void takeExample() {
+        Observable.range(1, 10).take(5)
+                .collect(StringBuilder::new, (builder, value) -> builder.append(value).append(" "))
+                .subscribe(list -> Logger.log("%s", list));
+    }
+
+    /**
+     * Skip first n elements.
+     * when n is below 0 then skip will be ignored.
+     */
+    private void skipExample() {
+        Observable.range(1, 10).skip(-99)
+                .collect(StringBuilder::new, (builder, value) -> builder.append(value).append(" "))
+                .subscribe(list -> Logger.log("%s", list));
+    }
+
+    private Observable<Integer> source() {
+        return Observable.range(1 << 6, 1 << 9);
+    }
+
+    private Single<String> joiner(Observable<Integer> source) {
+        return source.collect(StringBuilder::new, (builder, value) -> builder.append(value).append(" "))
+                .map(StringBuilder::toString);
+    }
+
+    private void firstLastExample() {
+        source().first(-1)
+                .map(f -> "First " + f)
+                .mergeWith(source().last(-1)
+                        .map(v -> "Last " + v))
+                .subscribe(v -> Logger.log("%s", v));
+
+    }
+
+    private void takeUntilExample(){
+        source().takeUntil(v -> v == 128)
+                .subscribe(v -> Logger.log("%d", v));
+    }
+
+    private void takeWhileExample(){
+        source().takeWhile(v -> v < 80)
+                .subscribe(v -> Logger.log("%d", v));
+    }
+
+    private void elementAtExample(){
+        List<Integer> ids = Arrays.asList(1, 3, 57, 62, 33, 24);
+        Observable.fromIterable(ids)
+                .elementAt(2)
+                .subscribe(id -> Logger.log("%d",id));
+    }
+
 }
