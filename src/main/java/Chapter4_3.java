cd0306c1bdfd2d1bee09b319253229db99b615b7
Dmitry Pashko <goodvin1709@gmail.com>
2018-05-09 01:38:49 +0300

    Using Rx Schedulers for creating task in future.

--- src/main/java/Chapter4_3.java
+++ src/main/java/Chapter4_3.java
@@ -1,11 +1,23 @@
+import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.PublishSubject;
 import io.reactivex.subjects.ReplaySubject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Chapter4_3 {
 
     Chapter4_3() {
 //        publishSubjectExample();
-        bufferingPublishSubjectExample();
+//        bufferingPublishSubjectExample();
+        try {
+            schedulerExample();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -41,4 +53,23 @@ class Chapter4_3 {
         subject.onNext("three");
         subject.onNext("four");
     }
+
+    private void schedulerExample() throws InterruptedException {
+        final Thread mainThread = Thread.currentThread();
+        final long startTime = Schedulers.newThread().now(TimeUnit.MILLISECONDS);
+        Logger.log("Start time : %d", startTime);
+        Schedulers.newThread()
+                .scheduleDirect(() -> {
+                    long endTime = System.currentTimeMillis();
+                    Logger.log("End time : %d", endTime);
+                    Logger.log("Process takes : %d", endTime - startTime);
+                    synchronized (mainThread) {
+                        mainThread.notify();
+                    }
+                }, 5000, TimeUnit.MILLISECONDS);
+        synchronized (mainThread) {
+            Thread.currentThread().wait();
+        }
+    }
+
 }
